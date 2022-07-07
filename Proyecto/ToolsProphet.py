14a40b3d6aa07d575e42bf1466e955690e508681
joankenobi <joan1233blanco@gmail.com>
2022-07-07 08:32:40 -0400

    704 datos prophet, empezar dummie y caterorization

--- Proyecto/ToolsProphet.py
+++ Proyecto/ToolsProphet.py
@@ -100,8 +100,8 @@ class ToolsProphet:
       if param_grid==None:
           param_grid = {
             #"growth":['linear','logistic'],
-            'changepoint_prior_scale': [0.05, 2.5],  # default 0.05 Increasing changepoint_prior_scale will make the trend more flexible and result in overfitting. Decreasing the changepoint_prior_scale will make the trend less flexible and result in underfitting.
-            'seasonality_prior_scale': [10],  # default 10
+            'changepoint_prior_scale': [0.05],  # default 0.05 Increasing changepoint_prior_scale will make the trend more flexible and result in overfitting. Decreasing the changepoint_prior_scale will make the trend less flexible and result in underfitting.
+            'seasonality_prior_scale': [10, 0.01],  # default 10
             'seasonality_mode': ["additive", "multiplicative"],
             #'daily_seasonality':[True,False]
             }
